7a99f2154478b86438b12d8f835673e0027e582c
khadar786 <khadarsayyad@gmail.com>
2023-10-26 18:58:43 +0530

    undefined vs not defined

---
+++ undefinedvsnotdefined/index.js
@@ -0,0 +1,14 @@
+//Not defined or undefined in javascript
+//console.log(a);
+
+if(a===undefined){
+  console.log("undefined");
+}else{
+  console.log("not undefined");
+}
+var a="khadar";
+console.log(a);
+
+console.log(b);
+
+(obj!==undefined || obj==null)?obj.name:"";
