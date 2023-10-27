155c081bfe53ef7f5dd45e404ff03f6eddf0334a
khadar786 <khadarsayyad@gmail.com>
2023-10-27 19:16:43 +0530

    week set & map

--- MapSetWeakMapWeakSet/index.js
+++ MapSetWeakMapWeakSet/index.js
@@ -21,7 +21,45 @@ console.log(setobj);
 //console.log(setobj);
 console.log(setobj.has(1))
 
-for(let v of obj1){
+for(let v of setobj){
   console.log(v);
 }
-//map -> key -> values
+
+setobj.forEach((ele)=>{
+  console.log(ele);
+});
+
+//Map -> key -> values
+let myMap=new Map([["a1","khadar"],["a2","basha"]]);
+myMap.set("a3","sayyad");
+
+console.log(myMap);
+console.log(myMap.get("a2"));
+
+for([key,value] of myMap){
+  console.log(`keys ${key},value ${value}`);
+}
+
+console.log("||||||||||||||||||||||||||||");
+myMap.forEach((ele,key)=>{
+  console.log(`keys ${key},value ${ele}`);
+});
+
+//WeakSet
+//only store objects
+//it cann't be iterated
+let ws=new WeakSet();
+var obj3={"name":"Tiger"};
+var obj4={};
+ws.add(obj3);
+ws.add(obj4);
+ws.delete(obj3);
+console.log(ws.has(obj3));
+
+//WeakMap
+let wm=new WeakMap();
+var obj5={"name":"khadar"};
+var obj6={};
+wm.set(obj5,"Private");
+wm.set(obj6,"Private");
+console.log(wm);
